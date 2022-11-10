647670f9fc0c458564576587360a11231e07ace4
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-11-11 03:43:40 +0600

    Added Require Auth & React Hot Toast

--- certificate-generator/src/App.jsx
+++ certificate-generator/src/App.jsx
@@ -2,9 +2,11 @@ import React, { useRef, useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import CertificateMain from './Component/CertificateMain/CertificateMain'
 import Home from './Component/Home/Home'
+import Loading from './Component/Loading/Loading'
 import Login from './Component/Login/Login'
 import NavMenu from './Component/NavMenu/NavMenu'
 import Register from './Component/Register/Register'
+import RequireAuth from './Component/RequireAuth/RequireAuth'
 
 
 function App() {
@@ -17,9 +19,12 @@ function App() {
       <hr />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="certificate-generator" element={<CertificateMain/>} />
-        <Route path="login" element={<Login/>} />
-        <Route path="register" element={<Register/>} />
+        <Route path="certificate-generator" element={<RequireAuth>
+          <CertificateMain />
+        </RequireAuth>} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path='loading' element={<Loading />} ></Route>
       </Routes>
     </div>
   )
--- certificate-generator/src/Component/RequireAuth/RequireAuth.jsx
+++ certificate-generator/src/Component/RequireAuth/RequireAuth.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
+import { useAuthState, useSendEmailVerification } from 'react-firebase-hooks/auth';
+import { Navigate, useLocation } from 'react-router-dom';
+import auth from '../firebase.init';
+import toast, { Toaster } from 'react-hot-toast';
+import Loading from '../Loading/Loading';
 
-const RequireAuth = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const RequireAuth = ({ children }) => {
+    const [user, loading] = useAuthState(auth);
+    const [sendEmailVerification, sending, error] = useSendEmailVerification(auth);
+    const location = useLocation();
+    if (loading) {
+        return <Loading></Loading>;
+    }
+    if (!user) {
+        return <Navigate to='/login' state={{ from: location }} replace></Navigate>
+    }
+    if (!user.emailVerified) {
+        return <div className='container mx-auto text-center h-[90vh] flex justify-center items-center'>
+            <div className="card w-80 border">
+                <div className="card-body py-5">
+                    <p>Your email is not verified</p>
+                    <p>Please verify your email</p>
+                    <button className='text-[#1C6DD0]' onClick={async () => {
+                        await sendEmailVerification();
+                        toast.success('Sent Email');
+                    }}>Resend</button>
+                </div>
+            </div>
+            <Toaster position="top-center" reverseOrder={false} />
+        </div>;
+    }
+    return children;
+
 };
 
 export default RequireAuth;
